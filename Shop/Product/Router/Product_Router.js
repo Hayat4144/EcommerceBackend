c730a6b3b4ff3370057c4fe5d026c85c2d4a6a17
hayat4144 <ihayat855@gmail.com>
2022-12-05 20:54:23 +0530

    product router is completed.

--- Shop/Product/Router/Product_Router.js
+++ Shop/Product/Router/Product_Router.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { CreateProduct } = require('../Api/CreateProduct');
+const { DeleteProduct } = require('../Api/DeleteProduct');
+const { Get_All_Product } = require('../Api/Get_All_Product');
+const { UpdateProduct } = require('../Api/UpdataProduct');
 const Product_router = express.Router();
 
 
@@ -7,6 +10,15 @@ const Product_router = express.Router();
 
 // 1. Create Product 
 Product_router.post('/v4/api/create/product' , CreateProduct)
+
+// 2. get all product 
+Product_router.get('/v4/api/get_all/product',Get_All_Product)
+
+// 3 Delete product 
+Product_router.delete('/v4/api/delete/product',DeleteProduct)
+
+// 4. Update Product
+Product_router.put('/v4/api/update/product',UpdateProduct)
 
 
 module.exports  = Product_router ;
