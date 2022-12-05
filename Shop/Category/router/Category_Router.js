160106d42b1c35439c881f6cbad1f45dace55650
hayat4144 <ihayat855@gmail.com>
2022-12-05 16:51:51 +0530

    Category route is completed.

--- Shop/Category/router/Category_Router.js
+++ Shop/Category/router/Category_Router.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { Create_Category } = require('../api/Create_Category');
+const { DeleteCategory } = require('../api/DeleteCategory');
 const { Get_Categories } = require('../api/Get_Categories');
 const Category_router = express.Router();
 
+//Rest Api 
+
+// 1. create category
 Category_router.post('/v4/api/create/category', Create_Category)
+
+// 2 . get category
 Category_router.get('/v4/api/get_all_categories',Get_Categories)
+
+// 3 . Delete category 
+Category_router.delete('/v4/api/delete/category', DeleteCategory)
 
 module.exports = Category_router ;
