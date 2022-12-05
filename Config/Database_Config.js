9fb3e1c02a97abd842a6341eeee30bd300158db0
hayat4144 <ihayat855@gmail.com>
2022-12-05 15:39:18 +0530

    dataase connection complete

--- Config/Database_Config.js
+++ Config/Database_Config.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
-exports.Database_Config =()=>{
-	mongoose.connect(process.env.DATABSE_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }, (err)=>{
-		err ? console.log(err) : console.log('connected') 
-	})
-} 
+const mongoose = require('mongoose')
+
+// database conncetion 
+const Database_Connect = () => {
+  mongoose.connect(process.env.DATABASE_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(console.log('Connected'))
+    .catch(err => console.log(err))
+
+}
+
+module.exports = Database_Connect;
