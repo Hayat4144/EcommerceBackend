c039c13e54d87186aa3da9a3e8181c8270ee5fc3
hayat4144 <ihayat855@gmail.com>
2022-12-05 20:11:12 +0530

    Product router is added.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const Database_Connect = require('./Config/Database_Config')
 const Category_router = require('./Shop/Category/router/Category_Router')
-
+const Product_router = require('./Shop/Product/Router/Product_Router')
 
 //connect database 
 Database_Connect();
@@ -13,6 +13,9 @@ app.use(express.json())
 
 // category_router 
 app.use(Category_router)
+
+// product router
+app.use(Product_router)
 
 app.listen(5000, (err) => {
 	err ? console.log(err) : console.log('running at port 500')
