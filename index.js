cf453fe8fb84d1579ed6c698b310344b8b47a6e8
hayat4144 <ihayat855@gmail.com>
2022-12-05 22:26:41 +0530

    seller route is added.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv').config();
 const Database_Connect = require('./Config/Database_Config')
 const Category_router = require('./Shop/Category/router/Category_Router')
 const Product_router = require('./Shop/Product/Router/Product_Router')
+const Seller_router = require('./Seller/auth/Router/SellerRouter')
+
 
 //connect database 
 Database_Connect();
@@ -16,6 +18,9 @@ app.use(Category_router)
 
 // product router
 app.use(Product_router)
+
+// seller router
+app.use(Seller_router)
 
 app.listen(5000, (err) => {
 	err ? console.log(err) : console.log('running at port 500')
