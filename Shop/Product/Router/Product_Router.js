8a565b411d57a458173abbb798eb5559f8844072
hayat4144 <ihayat855@gmail.com>
2022-12-06 18:55:24 +0530

    product and create category is completed.

--- Shop/Product/Router/Product_Router.js
+++ Shop/Product/Router/Product_Router.js
@@ -3,22 +3,23 @@ const { CreateProduct } = require('../Api/CreateProduct');
 const { DeleteProduct } = require('../Api/DeleteProduct');
 const { Get_All_Product } = require('../Api/Get_All_Product');
 const { UpdateProduct } = require('../Api/UpdataProduct');
+const SellerAuthMiddleware = require('../../../Middleware/SellerAuthMiddleware')
 const Product_router = express.Router();
 
 
 // Product Routers
 
 // 1. Create Product 
-Product_router.post('/v4/api/create/product' , CreateProduct)
+Product_router.post('/v4/api/create/product' , SellerAuthMiddleware,CreateProduct)
 
 // 2. get all product 
-Product_router.get('/v4/api/get_all/product',Get_All_Product)
+Product_router.get('/v4/api/get_all/product',SellerAuthMiddleware,Get_All_Product)
 
 // 3 Delete product 
-Product_router.delete('/v4/api/delete/product',DeleteProduct)
+Product_router.delete('/v4/api/delete/product',SellerAuthMiddleware,DeleteProduct)
 
 // 4. Update Product
-Product_router.put('/v4/api/update/product',UpdateProduct)
+Product_router.put('/v4/api/update/product',SellerAuthMiddleware,UpdateProduct)
 
 
 module.exports  = Product_router ;
