875fbb93410552ea8a50290bd7dc5216046c1671
hayat4144 <ihayat855@gmail.com>
2022-12-06 18:58:17 +0530

    body parser is confi.g

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const body_pareser = require('body-parser')
+const cookie_parser = require('cookie-parser') ;
 const Database_Connect = require('./Config/Database_Config')
 const Category_router = require('./Shop/Category/router/Category_Router')
 const Product_router = require('./Shop/Product/Router/Product_Router')
-const Seller_router = require('./Seller/auth/Router/SellerRouter')
+const Seller_router = require('./Seller/Router/SellerRouter');
+const { ErrorMiddleware } = require('./Middleware/ErrorMiddleware');
 
 
 //connect database 
 Database_Connect();
 
-// express json parser 
-app.use(express.json())
+// body_parser
+app.use(body_pareser.json())
+
+// cookie-parser 
+app.use(cookie_parser())
+
 
 // category_router 
 app.use(Category_router)
@@ -21,6 +28,9 @@ app.use(Product_router)
 
 // seller router
 app.use(Seller_router)
+
+app.use(ErrorMiddleware)
+
 
 app.listen(5000, (err) => {
 	err ? console.log(err) : console.log('running at port 500')
