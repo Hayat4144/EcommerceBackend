4e6fde61b040d83ead70462aa1bc94eb04945bc1
hayat4144 <ihayat855@gmail.com>
2022-12-06 18:57:00 +0530

    category router is changed.

--- Shop/Category/router/Category_Router.js
+++ Shop/Category/router/Category_Router.js
@@ -3,11 +3,12 @@ const { Create_Category } = require('../api/Create_Category');
 const { DeleteCategory } = require('../api/DeleteCategory');
 const { Get_Categories } = require('../api/Get_Categories');
 const Category_router = express.Router();
+const SellerAuthMiddleware = require('../../../Middleware/SellerAuthMiddleware')
 
 //Rest Api 
 
 // 1. create category
-Category_router.post('/v4/api/create/category', Create_Category)
+Category_router.post('/v4/api/create/category', SellerAuthMiddleware, Create_Category)
 
 // 2 . get category
 Category_router.get('/v4/api/get_all_categories',Get_Categories)
