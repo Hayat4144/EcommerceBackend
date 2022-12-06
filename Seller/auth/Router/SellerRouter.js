898073e0361cf8cef3b937ebd35fbefd10d36f72
hayat4144 <ihayat855@gmail.com>
2022-12-06 12:30:28 +0530

    doesnot exist

--- Seller/auth/Router/SellerRouter.js
+++
@@ -1,9 +0,0 @@
-const epxress = require('express');
-const { SignupSeller } = require('../api/SignupSeller');
-const SellerRouter = epxress.Router();
-
-// 1. Signup for Seller route
-SellerRouter.post('/v4/api/seller/signup', SignupSeller)
-
-
-module.exports = SellerRouter ;
