089147095ba844a976a308aaeb4dbf925ac00b51
hayat4144 <ihayat855@gmail.com>
2022-12-06 21:13:17 +0530

    product varient model is completed.

---
+++ Shop/Product/model/Product_Varient.js
@@ -0,0 +1,44 @@
+
+const mongoose = require('mongoose')
+require('mongoose-type-url')
+
+const ProductVarientSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    stock: {
+        type: Number,
+        min: 10,
+        max: 400,
+        required: true
+    },
+    image: {
+        type: mongoose.SchemaTypes.Url,
+        required: true
+    },
+    price:{
+        type:Number,
+        required:true,
+        min:1  
+    },
+    seller:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:'seller'
+    },
+    product_attribute: {
+        type: Array,
+        required: true
+    }
+},
+    {
+        timestamps: {
+            createdAt: 'created_at', // Use `created_at` to store the created date
+            updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+        }
+    }
+)
+
+const ProductVarient_Model = new mongoose.model('product_varients' , ProductVarientSchema)
+module.exports = ProductVarient_Model
