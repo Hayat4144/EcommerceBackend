fa068f516bd4298ab14813f35b8c69207b694cf5
hayat4144 <ihayat855@gmail.com>
2022-12-06 21:12:27 +0530

    Product router is completed.

--- Shop/Product/Router/Product_Router.js
+++ Shop/Product/Router/Product_Router.js
@@ -3,7 +3,11 @@ const { CreateProduct } = require('../Api/CreateProduct');
 const { DeleteProduct } = require('../Api/DeleteProduct');
 const { Get_All_Product } = require('../Api/Get_All_Product');
 const { UpdateProduct } = require('../Api/UpdataProduct');
-const SellerAuthMiddleware = require('../../../Middleware/SellerAuthMiddleware')
+const SellerAuthMiddleware = require('../../../Middleware/SellerAuthMiddleware');
+const { CreateProductVarient } = require('../Api/varients/CrProductVarient');
+const { Get_All_ProductVarients, fetch_Product_By_Id_Or_ProductId } = require('../Api/varients/FetchProductVarients');
+const { DeleteVarients } = require('../Api/varients/DltVarients');
+const { UpdataVarients } = require('../Api/varients/UpdVarients');
 const Product_router = express.Router();
 
 
@@ -21,5 +25,22 @@ Product_router.delete('/v4/api/delete/product',SellerAuthMiddleware,DeleteProduc
 // 4. Update Product
 Product_router.put('/v4/api/update/product',SellerAuthMiddleware,UpdateProduct)
 
+//  Product Varients
+
+// 5. Create Product varient
+Product_router.post('/v4/api/create/product/varient', SellerAuthMiddleware,CreateProductVarient)
+
+
+// 6. fetch prduct varient
+Product_router.get('/v4/api/get_all/product/varients', SellerAuthMiddleware,Get_All_ProductVarients)
+
+//  7 . fetch product by varient id or product id
+Product_router.get('/v4/api/get_product_varient/' ,SellerAuthMiddleware,fetch_Product_By_Id_Or_ProductId)
+
+//  8 delete product varient
+Product_router.delete('/v4/api/delete/product/varient', SellerAuthMiddleware,DeleteVarients)
+
+// 9 update product varient
+Product_router.put('/v4/api/update/product/varient' ,SellerAuthMiddleware,UpdataVarients)
 
 module.exports  = Product_router ;
