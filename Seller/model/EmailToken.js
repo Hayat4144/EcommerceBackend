9360b1c81ebf7888befe90eb15dc72b2782a0a56
hayat4144 <ihayat855@gmail.com>
2022-12-08 13:07:16 +0530

    token model fo reset passaword.

---
+++ Seller/model/EmailToken.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const EmailTokenShema = new mongoose.SchemaType({
+    seller: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'seller',
+        required: true,
+        unique: true
+    },
+    token: {
+        type: String,
+        required: true,
+        uniqued: true
+    }
+},
+    {
+        timestamps: {
+            createdAt: 'created_at', // Use `created_at` to store the created date
+            updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+        }
+    }
+)
+
+const EmailTokenModel = new mongoose.model('Token', EmailTokenShema)
+
+module.exports = EmailTokenModel ;
