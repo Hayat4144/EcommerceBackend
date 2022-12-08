f9660343752a9910e2204f597170565b68f05c01
hayat4144 <ihayat855@gmail.com>
2022-12-08 18:24:07 +0530

    Signin and signup is completed for seller account.

---
+++ Seller/auth/SignupSeller.js
@@ -0,0 +1,26 @@
+const SellerModel = require('../model/SellerModel')
+const bcrypt = require('bcrypt')
+
+exports.SignupSeller = async (req, res) => {
+    try {
+        const { name, store_name, email, password } = req.body;
+        const saltRound = 10;
+        const hashpassword = await bcrypt.hash(password, saltRound);
+        const create_seller = {
+            name, store_name, email, password:hashpassword
+        }
+        await SellerModel.create(create_seller,(error,doc)=>{
+            if(!error){
+                return res.status(200).json({"data":`Seller ${doc.email} has been created Successfully.`})
+            }
+            else{
+                if(error.code === 11000){
+                    return res.status(400).json({"error":`Sorry, User ${error.keyValue.email} is already exist.`})
+                }
+                return res.status(400).json({error})
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
