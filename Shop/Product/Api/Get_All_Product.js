06ac832e649dc574d06e2ed20f6a8466b2f25991
hayat4144 <ihayat855@gmail.com>
2022-12-08 13:02:12 +0530

    proudct api is complted.

--- Shop/Product/Api/Get_All_Product.js
+++ Shop/Product/Api/Get_All_Product.js
@@ -1,11 +1,61 @@
 const Product_Model = require('../model/Product_Model')
 
-exports.Get_All_Product = async(req,res)=>{
+exports.Get_All_Product = async (req, res) => {
     try {
-        await Product_Model.find({seller:req.user_id})
-        .exec((error,doc)=>{
-            return error ? res.status(400).json({error}) : res.status(200).json({doc})
-        })
+        let { search, sort, category, page } = req.query;
+        if (req.query.price) {
+            var Price = JSON.stringify(req.query.price);
+            Price = Price.replace(/\b(gt|lt|gte|lte)\b/g, key => `$${key}`)
+            Price = JSON.parse(Price)
+        }
+
+        if (search) {
+            search = {
+                seller: req.user_id,
+                name: { $regex: search, $options: 'i' },
+
+            }
+        }
+        else {
+
+            search = {
+                // seller: req.user_id,
+                // price: Price
+            }
+        }
+
+        const pagination = (resultPerPage) => {
+            page = page || 1
+            const skip = resultPerPage * (page - 1)
+            return skip;
+        }
+
+
+        if (req.query.sort) {
+            var SortIn = {};
+            sort = sort.split(',')
+            if (req.query.orderby === 'desc') {
+                sort.forEach(key => {
+                    SortIn[key] = -1
+                });
+            }
+            else {
+                sort.forEach(key => {
+                    SortIn[key] = 1
+                });
+            }
+        }
+        await Product_Model.find(search)
+            .skip(pagination(5))
+            .limit(5)
+            .sort(SortIn)
+            .populate({
+                path: 'category',
+                match: { name: { $regex: req.query.category, $options: 'i' } }
+            })
+            .exec((error, doc) => {
+                return error ? res.status(400).json({ error }) : res.status(200).json({ doc })
+            })
     } catch (error) {
         console.log(error)
     }
