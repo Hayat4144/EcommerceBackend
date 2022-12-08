3a27d35bb9f670f49b2863103093cb38b3816bfe
hayat4144 <ihayat855@gmail.com>
2022-12-08 18:26:29 +0530

    UserModel is completed.

---
+++ Users/Model/UserModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+require('mongoose-type-url')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        max: 20,
+        min: 4
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 20,
+        min: 8
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        timestamps: {
+            createdAt: 'created_at', // Use `created_at` to store the created date
+            updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+        }
+    }
+)
+
+
+const UserModel = new mongoose.model('user', UserSchema)
+module.exports = UserModel;
