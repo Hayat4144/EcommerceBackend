a4a6f1d4d650376a8a68844a9bdfd5a68b4773e8
hayat4144 <ihayat855@gmail.com>
2022-12-08 18:24:32 +0530

    CastError handled.

--- Middleware/ErrorMiddleware.js
+++ Middleware/ErrorMiddleware.js
@@ -1,8 +1,12 @@
 const ErrorHandler = require('../utils/ErrorHandler');
 
-exports.ErrorMiddleware= (err,req,res ,next)=>{
-    err.statusCode = err.statusCode || 500 
-    err.message = err.message  || 'Internal server'
-    res.status(err.statusCode).json({error:err.message})
+exports.ErrorMiddleware = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.message = err.message || 'Internal server'
+    if (err.name === 'CastError') {
+        const message = `Resource not found: ${err.path} `
+        err = new ErrorHandler(message, 400)
+    }
+    res.status(err.statusCode).json({ error: err.message })
 }
 
