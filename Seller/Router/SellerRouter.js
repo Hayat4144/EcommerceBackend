842a4760ba36bc3ba89c2e45731e266ddf43dd5f
hayat4144 <ihayat855@gmail.com>
2022-12-08 18:26:56 +0530

    seller router is complete for auth

--- Seller/Router/SellerRouter.js
+++ Seller/Router/SellerRouter.js
@@ -1,13 +1,16 @@
 const epxress = require('express');
-const { SigninSeller } = require('../auth/api/SigninSelller');
-const { SignupSeller } = require('../auth/api/SignupSeller');
+const { SigninSeller } = require('../auth/SigninSelller');
+const { SignupSeller } = require('../auth/SignupSeller');
+const SellerAuthMiddleware = require('../../Middleware/SellerAuthMiddleware');
+const { ChangePassword } = require('../auth/ChangePassword');
 const SellerRouter = epxress.Router();
 
 // 1. Signup for Seller route
 SellerRouter.post('/v4/api/seller/signup', SignupSeller)
 
 // 2 Signin for seller route
-SellerRouter.post('/v4/api/seller/signin',SigninSeller)
+SellerRouter.post('/v4/api/seller/signin', SigninSeller)
 
-
-module.exports = SellerRouter ;
+// 3 change password for seller
+SellerRouter.put('/v4/api/seller/change/password', SellerAuthMiddleware, ChangePassword)
+module.exports = SellerRouter;
