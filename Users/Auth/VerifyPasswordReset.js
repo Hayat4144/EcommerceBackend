a1904e9472f3788ad4123376ea448abaf35159bf
hayat4144 <ihayat855@gmail.com>
2022-12-09 20:33:20 +0530

    Bug fixed.

--- Users/Auth/VerifyPasswordReset.js
+++ Users/Auth/VerifyPasswordReset.js
@@ -10,18 +10,21 @@ exports.VerifyPasswordChange = AsyncFunc(async (req, res, next) => {
     if (IsValidToken.length === 0) {
         next(new ErrorHandler(`Sorry , Invalid/Broken link or token`, 400))
     }
-    const saltRound = 10;
-    const hashpassword = await bcrypt.hash(new_password, saltRound);
-    await UserModel.findByIdAndUpdate(req.user_id, { $set: { password: hashpassword } }, { $new: true })
-        .exec((error, doc) => {
-            if (error) {
-                next(new ErrorHandler(error.message, 400))
-            }
-            else {
-                if (doc === null) {
-                    return res.status(400).json({ 'error': `Sorry , User does not exist` })
+    else {
+        const saltRound = 10;
+        const hashpassword = await bcrypt.hash(new_password, saltRound);
+        await UserModel.findByIdAndUpdate(req.user_id, { $set: { password: hashpassword } }, { $new: true })
+            .exec((error, doc) => {
+                if (error) {
+                    next(new ErrorHandler(error.message, 400))
                 }
-                return res.status(200).json({ data: `The password has been changed successfully .` })
-            }
-        })
+                else {
+                    if (doc === null) {
+                        return res.status(400).json({ 'error': `Sorry , User does not exist` })
+                    }
+                    return res.status(200).json({ data: `The password has been changed successfully .` })
+                }
+            })
+    }
+
 })
