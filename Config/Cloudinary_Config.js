8f2bf13d77f4b48fa0c43ad75793bff72c1b9a67
hayat4144 <ihayat855@gmail.com>
2022-12-10 16:23:31 +0530

    conigure cloudinary.

---
+++ Config/Cloudinary_Config.js
@@ -0,0 +1,11 @@
+const cloudinary = require("cloudinary").v2;
+
+
+exports.CloudinaryConfiguration = () => {
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        secure: true,
+    });
+}
