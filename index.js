1e4cf6bd2df191ad90771f22983b981f1f14faaf
hayat4144 <ihayat855@gmail.com>
2022-12-15 19:58:55 +0530

    cors installed and configure.

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config();
-const body_pareser = require('body-parser')
-const cookie_parser = require('cookie-parser');
-const Database_Connect = require('./Config/Database_Config')
-const Category_router = require('./Shop/Category/router/Category_Router')
-const Product_router = require('./Shop/Product/Router/Product_Router')
-const Seller_router = require('./Seller/Router/SellerRouter');
-const { ErrorMiddleware } = require('./Middleware/ErrorMiddleware');
-const UserRouter = require('./Users/Router/UserRouter');
-const { CloudinaryConfiguration } = require('./Config/Cloudinary_Config');
+const dotenv = require("dotenv").config();
+const body_pareser = require("body-parser");
+const cookie_parser = require("cookie-parser");
+const Database_Connect = require("./Config/Database_Config");
+const Category_router = require("./Shop/Category/router/Category_Router");
+const Product_router = require("./Shop/Product/Router/Product_Router");
+const Seller_router = require("./Seller/Router/SellerRouter");
+const { ErrorMiddleware } = require("./Middleware/ErrorMiddleware");
+const UserRouter = require("./Users/Router/UserRouter");
+const { CloudinaryConfiguration } = require("./Config/Cloudinary_Config");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // configure clodinay
 CloudinaryConfiguration();
 
-
-//connect database 
+//connect database
 Database_Connect();
 
-
 // body_parser
-app.use(body_pareser.json())
+app.use(body_pareser.json());
 
-// cookie-parser 
-app.use(cookie_parser())
+// cookie-parser
+app.use(cookie_parser());
 
-
-// category_router 
-app.use(Category_router)
+// category_router
+app.use(Category_router);
 
 // product router
-app.use(Product_router)
+app.use(Product_router);
 
 // seller router
-app.use(Seller_router)
+app.use(Seller_router);
 
-// user router 
-app.use(UserRouter)
+// user router
+app.use(UserRouter);
 
-app.use(ErrorMiddleware)
-
+app.use(ErrorMiddleware);
 
 const server = app.listen(5000, (err) => {
-	err ? console.log(err) : console.log('running at port 500')
-})
-
+  err ? console.log(err) : console.log("running at port 500");
+});
 
 // unhandledRejection
 // process.on('unhandledRejection', (err) => {
