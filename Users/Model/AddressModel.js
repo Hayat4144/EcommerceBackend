257cada25fb8b85893793cfe55f1274051a03021
hayat4144 <ihayat855@gmail.com>
2022-12-17 12:46:08 +0530

    unique property added in user field.

--- Users/Model/AddressModel.js
+++ Users/Model/AddressModel.js
@@ -1,55 +1,55 @@
 const mongoose = require('mongoose')
 
 const AddressSchema = new mongoose.Schema({
-	user:{
-		type:mongoose.SchemaTypes.ObjectId,
-		required:true,
-		unique:true,
-		ref:'user'
+	user: {
+		type: mongoose.SchemaTypes.ObjectId,
+		required: true,
+		unique: true,
+		ref: 'user'
 	},
-	Street:{
-		type:String,
-		required:true,
-		max:50,
+	Street: {
+		type: String,
+		required: true,
+		max: 50,
 	},
-	Area:{
-		type:String,
-		required:true,
-		max:50
+	Area: {
+		type: String,
+		required: true,
+		max: 50
 	},
-	city:{
-		type:String,
-		required:true,
-		max:20
+	city: {
+		type: String,
+		required: true,
+		max: 20
 	},
-	State:{
-		type:String,
-		required:true,
-		max:30
+	State: {
+		type: String,
+		required: true,
+		max: 30
 	},
-	pincode:{
-		type:Number,
-		required:true,
-		min:6,
-		
+	pincode: {
+		type: Number,
+		required: true,
+		min: 6,
+
 	},
-	Country:{
-		type:String,
-		required:true,
-		max:20,
-		min:4
+	Country: {
+		type: String,
+		required: true,
+		max: 20,
+		min: 4
 	}
-	
-} ,
- {
-        timestamps: {
-            createdAt: 'created_at', // Use `created_at` to store the created date
-            updatedAt: 'updated_at' // and `updated_at` to store the last updated date
-        }
-    }
+
+},
+	{
+		timestamps: {
+			createdAt: 'created_at', // Use `created_at` to store the created date
+			updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+		}
+	}
 )
 
 
-const AddressUserModel = mongoose.model('useraddress',AddressSchema)
+const AddressUserModel = mongoose.model('useraddress', AddressSchema)
 
-module.exports = AddressUserModel ;
+module.exports = AddressUserModel;
