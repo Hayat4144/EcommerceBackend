d46d0a29749c9cc42bad4214e54884f4cc1e45b9
hayat4144 <ihayat855@gmail.com>
2022-12-17 16:33:12 +0530

    validation for account create and address is completed.

---
+++ Users/Validation/UserValidation.js
@@ -0,0 +1,30 @@
+const { check, validationResult } = require('express-validator')
+
+
+exports.UserValidate = [
+    check('password')
+        .isStrongPassword()
+        .trim()
+        .escape()
+        .withMessage('password must be contain symbol,number,capital and small letter.'),
+    check('name')
+        .isLength({ min: 4, max: 15 })
+        .trim()
+        .escape()
+        .withMessage('Name should be minimum 4 and maximum 15 character.'),
+    check('email')
+        .isEmail()
+        .trim()
+        .escape()
+        .withMessage('Invalid email'),
+]
+
+exports.User_Validation_Error = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ error: errors.array()[0].msg })
+    }
+    else {
+        next();
+    }
+}
