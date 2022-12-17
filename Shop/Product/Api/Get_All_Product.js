77c74e3baf589e8c365c37b307e44d70378f10b8
hayat4144 <ihayat855@gmail.com>
2022-12-17 20:42:28 +0530

    aggregations pipeline used for search filter and pagination of products.

--- Shop/Product/Api/Get_All_Product.js
+++ Shop/Product/Api/Get_All_Product.js
@@ -5,22 +5,10 @@ exports.Get_All_Product = async (req, res) => {
         let { search, sort, category, page } = req.query;
         if (req.query.price) {
             var Price = JSON.stringify(req.query.price);
-            Price = Price.replace(/\b(gt|lt|gte|lte)\b/g, key => `$${key}`)
+            Price = Price.replace(/\b(gt|lt|gte|lte)\b/g, (key, value) => `$${key}`)
             Price = JSON.parse(Price)
-        }
-
-        if (search) {
-            search = {
-                seller: req.user_id,
-                name: { $regex: search, $options: 'i' },
-
-            }
-        }
-        else {
-
-            search = {
-                // seller: req.user_id,
-                // price: Price
+            for (let [key, value] of Object.entries(Price)) {
+                Price[key] = Number(value)
             }
         }
 
@@ -45,17 +33,32 @@ exports.Get_All_Product = async (req, res) => {
                 });
             }
         }
-        await Product_Model.find(search)
-            .skip(pagination(5))
-            .limit(5)
-            .sort(SortIn)
-            .populate({
-                path: 'category',
-                match: { name: { $regex: req.query.category, $options: 'i' } }
-            })
-            .exec((error, doc) => {
-                return error ? res.status(400).json({ error }) : res.status(200).json({ doc })
-            })
+        // await Product_Model.find(search)
+        //     .skip(pagination(5))
+        //     .limit(5)
+        //     .sort(SortIn)
+        //     .exec((error, doc) => {
+        //         console.log(error)
+        //         return error ? res.status(400).json({ error }) : res.status(200).json({ doc })
+        //     })
+
+        const t = { price: { $gt: 34, $lt: 45 } }
+        console.log(Price)
+        const data = await Product_Model.aggregate([
+            { $match: { name: { $regex: search, $options: 'i' } } },
+            { $match: { price: Price } },
+            {
+                $facet: {
+                    result: [{ $skip: pagination(5) }, { $limit: 10 }],
+                    total_result: [
+                        {
+                            $count: 'count'
+                        }
+                    ]
+                }
+            }
+        ])
+        return res.status(200).json({ data })
     } catch (error) {
         console.log(error)
     }
