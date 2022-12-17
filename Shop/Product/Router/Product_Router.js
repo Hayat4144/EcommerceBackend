3b1fe33216f117cb9485d82e750249af4f76e4ed
hayat4144 <ihayat855@gmail.com>
2022-12-17 12:03:26 +0530

    route redifined.

--- Shop/Product/Router/Product_Router.js
+++ Shop/Product/Router/Product_Router.js
@@ -14,33 +14,33 @@ const Product_router = express.Router();
 // Product Routers
 
 // 1. Create Product 
-Product_router.post('/v4/api/create/product' , SellerAuthMiddleware,CreateProduct)
+Product_router.post('/v4/api/create/product', SellerAuthMiddleware, CreateProduct)
 
 // 2. get all product 
-Product_router.get('/v4/api/get_all/product',SellerAuthMiddleware,Get_All_Product)
+Product_router.get('/v4/api/get_all/product', Get_All_Product)
 
 // 3 Delete product 
-Product_router.delete('/v4/api/delete/product',SellerAuthMiddleware,DeleteProduct)
+Product_router.delete('/v4/api/delete/product', SellerAuthMiddleware, DeleteProduct)
 
 // 4. Update Product
-Product_router.put('/v4/api/update/product',SellerAuthMiddleware,UpdateProduct)
+Product_router.put('/v4/api/update/product', SellerAuthMiddleware, UpdateProduct)
 
 //  Product Varients
 
 // 5. Create Product varient
-Product_router.post('/v4/api/create/product/varient', SellerAuthMiddleware,CreateProductVarient)
+Product_router.post('/v4/api/create/product/varient', SellerAuthMiddleware, CreateProductVarient)
 
 
 // 6. fetch prduct varient
-Product_router.get('/v4/api/get_all/product/varients', SellerAuthMiddleware,Get_All_ProductVarients)
+Product_router.get('/v4/api/get_all/product/varients', SellerAuthMiddleware, Get_All_ProductVarients)
 
 //  7 . fetch product by varient id or product id
-Product_router.get('/v4/api/get_product_varient/' ,SellerAuthMiddleware,fetch_Product_By_Id_Or_ProductId)
+Product_router.get('/v4/api/get_product_varient/', SellerAuthMiddleware, fetch_Product_By_Id_Or_ProductId)
 
 //  8 delete product varient
-Product_router.delete('/v4/api/delete/product/varient', SellerAuthMiddleware,DeleteVarients)
+Product_router.delete('/v4/api/delete/product/varient', SellerAuthMiddleware, DeleteVarients)
 
 // 9 update product varient
-Product_router.put('/v4/api/update/product/varient' ,SellerAuthMiddleware,UpdataVarients)
+Product_router.put('/v4/api/update/product/varient', SellerAuthMiddleware, UpdataVarients)
 
-module.exports  = Product_router ;
+module.exports = Product_router;
