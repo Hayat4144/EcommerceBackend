21c160bbb52c7e9bde5810a621ae224e7cf1882b
hayat4144 <ihayat855@gmail.com>
2022-12-17 20:36:21 +0530

    timestamps is added in category model.

--- Shop/Category/model/category_model.js
+++ Shop/Category/model/category_model.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose')
 
 const CategorySchema = new mongoose.Schema({
-	name:{
-		type:String,
-		required:false,
-		max:20
+	name: {
+		type: String,
+		required: false,
+		max: 20
 	},
-	slug:{
-		type:String,
-		required:true,
-		unique:true
+	slug: {
+		type: String,
+		required: true,
+		unique: true
 
 	},
-	parentId:{
-		type:String,
-		required:false,
-		
-	},
-	created_at:{
-		type:Date,
-		default: Date.now(),
+	parentId: {
+		type: String,
+		required: false,
+
 	}
-
-
-})
+},
+	{
+		timestamps: {
+			createdAt: 'created_at',
+			updatedAt: 'updated_at'
+		}
+	})
 
 const category_model = new mongoose.model('Category', CategorySchema)
 
-module.exports = category_model ;
+module.exports = category_model;
