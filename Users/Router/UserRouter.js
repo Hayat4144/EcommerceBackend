f7fcb88440ccbe994145d963d4f1da12755bc676
hayat4144 <ihayat855@gmail.com>
2022-12-17 16:33:52 +0530

    user validtion function added in routes.

--- Users/Router/UserRouter.js
+++ Users/Router/UserRouter.js
@@ -12,12 +12,18 @@ const { ReadUserAddress } = require('../Address/api/ReadUserAddress');
 const { UpdateUserAddress } = require('../Address/api/UpdateUserAddress');
 const { UserProfileImage } = require('../Profile/userprofile_upload');
 const singalfileupload = require('../../utils/multe_singal');
+const { AddressValidation, Address_validation_Error } = require('../Validation/AddressValidations');
+const { UserValidate, User_Validation_Error } = require('../Validation/UserValidation');
+
+
+
+
 const UserRouter = express.Router();
 
 // User router
 
 // 1. Signup for user
-UserRouter.post('/v3/api/user/signup', UserSignup)
+UserRouter.post('/v3/api/user/signup', UserValidate, User_Validation_Error, UserSignup)
 
 // 2 Sigin for user
 UserRouter.post('/v3/api/user/signin', UserSignin)
@@ -38,7 +44,7 @@ UserRouter.post('/v3/api/user/change/email/request', UserAuthMiddleware, UserEma
 UserRouter.put('/v3/api/user/change/email/verify/done', UserAuthMiddleware, VerifyEmailChange)
 
 // 6 user address create 
-UserRouter.post('/v3/api/user/create/address', UserAuthMiddleware, CreateUserAddress)
+UserRouter.post('/v3/api/user/create/address', AddressValidation, Address_validation_Error, UserAuthMiddleware, CreateUserAddress)
 
 UserRouter.get('/v3/api/user/read/address', UserAuthMiddleware, ReadUserAddress)
 
