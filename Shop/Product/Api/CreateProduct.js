ab334ade2d69d7962fe05a31a706a140418a6af4
hayat4144 <ihayat855@gmail.com>
2022-12-21 16:02:53 +0530

    product pic upload functionality added.

--- Shop/Product/Api/CreateProduct.js
+++ Shop/Product/Api/CreateProduct.js
@@ -1,20 +1,64 @@
+const AsyncFunc = require('../../../utils/AsyncFunc');
 const Product_model = require('../model/Product_Model')
-exports.CreateProduct = async(req,res)=>{
-    try {
-        const {name,description,price,image,category,brand} = req.body ;
-        await Product_model.create({
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+const ErrorHandler = require('../../../utils/ErrorHandler')
+
+
+
+exports.CreateProduct = AsyncFunc(async (req, res, next) => {
+    const { name, description, cnt, price, varients, category, brand, attributes_name } = req.body;
+    const image_files = req.files;
+    const ImageUploadPromise = image_files.map(item => {
+        return new Promise((resolve, reject) => {
+            cloudinary.uploader.upload(item.path, { unique_filename: true, folder: "Shop" }, (error, data) => {
+                if (error) {
+                    next(new ErrorHandler(error.message, 400))
+                    return reject({ error })
+                }
+                else {
+                    return resolve(data.secure_url)
+                }
+            })
+        })
+
+    })
+
+    await Promise.all(ImageUploadPromise).then((data) => {
+        let attributes = []
+        attributes_name.forEach(element => {
+            attributes.push({
+                name: element
+            })
+        });
+        Product_model.create({
             name,
             description,
             price,
-            image,
             category,
             brand,
-            seller:req.user_id
-        },(err,doc)=>{
-            console.log(doc.seller)
-            return !err ? res.status(200).json({doc}) : res.status(400).json({err})
+            assets: {
+                images: data
+            },
+            varients: {
+                cnt,
+                attributes
+            },
+            seller: req.user_id
+        }, (error, data) => {
+            if (error) {
+                next(new ErrorHandler(error.message, 400))
+            } else {
+                return res.status(200).json({ data })
+            }
         })
-    } catch (error) {
-        console.log(error)
-    }
-}
+    }).catch((error) => {
+        res.status(400).json({ error })
+    })
+
+
+
+
+
+
+}) 
