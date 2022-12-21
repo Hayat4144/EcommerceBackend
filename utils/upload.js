aac98cfae6a4e9ca6060787ba32782d75b5c28d2
hayat4144 <ihayat855@gmail.com>
2022-12-21 16:01:20 +0530

    used diskstorage for multer.

--- utils/upload.js
+++ utils/upload.js
@@ -1,5 +1,9 @@
 const multer = require('multer')
-const storage = multer.memoryStorage();
+
+
+const storage = multer.diskStorage({
+    destination: '../uploads'
+});
 const limits = { fileSize: 1000 * 1000 * 1 }; // limit to 4mb
 const upload = multer({ storage })
 
