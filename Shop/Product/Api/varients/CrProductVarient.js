3ca6cafe9486239f1c2c807c1e375e212c270550
hayat4144 <ihayat855@gmail.com>
2022-12-22 12:51:05 +0530

    create varient logic is re-written.

--- Shop/Product/Api/varients/CrProductVarient.js
+++ Shop/Product/Api/varients/CrProductVarient.js
@@ -1,21 +1,41 @@
-const ProductVarient_Model = require('../..//model/Product_Varient')
-const ErrorHandler = require('../../../../utils/ErrorHandler')
-
-exports.CreateProductVarient = async (req, res, next) => {
-    try {
-        const {price, product, stock, image, product_attribute } = req.body;
-        await ProductVarient_Model.create({
-            product,
-            price,
-            image,
-            stock,
-            product_attribute,
-            seller: req.user_id
-        }, (err, doc) => {
-            console.log(doc.seller)
-            return !err ? res.status(200).json({ doc }) : res.status(400).json({ err })
+const ProductVarient_Model = require("../..//model/Product_Varient");
+const ErrorHandler = require("../../../../utils/ErrorHandler");
+const AsyncFunc = require("../../../../utils/AsyncFunc");
+const cloudinary = require('cloudinary').v2
+exports.CreateProductVarient = AsyncFunc(async (req, res, next) => {
+    const Data = JSON.parse(req.body.data);
+    const image_files = req.files;
+    const ImageUploadPromise = image_files.map((item) => {
+        return new Promise((resolve, reject) => {
+            cloudinary.uploader.upload(item.path, { folder: 'Shop' }, (error, data) => {
+                if (error) {
+                    next(new ErrorHandler(error.message, 400))
+                    return reject({ error })
+                }
+                else {
+                    return resolve(data.secure_url)
+                }
+            })
         })
-    } catch (error) {
-        next(new ErrorHandler(error.message, 500))
-    }
-}
+    })
+    await Promise.all(ImageUploadPromise).then((data) => {
+        ProductVarient_Model.create({
+            stock: Data.stock,
+            price: Data.price,
+            product: Data.product,
+            assets: {
+                images: data
+            },
+            product_attribute: Data.attributes,
+            seller: req.user_id
+        }, (error, data) => {
+            if (error) {
+                next(new ErrorHandler(error.message, 400))
+            } else {
+                return res.status(200).json({ data })
+            }
+        })
+    }).catch((error) => {
+        res.status(400).json({ error })
+    })
+});
