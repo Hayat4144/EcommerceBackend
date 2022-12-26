16b52d8b26577c99df2dc6baa3e7559beee03bb0
hayat4144 <ihayat855@gmail.com>
2022-12-26 15:46:45 +0530

    cart items model is added.

---
+++ Users/Model/CartItemModal.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const CartItemSchema = new mongoose.Schema({
+    cartId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref: 'cart'
+    },
+    ProductvarientId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Please Provide the Quantity"],
+        max: [500, "You should have to buy maximum 500"],
+        min: [5, "You should have to buy atleast 5"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please provide the price"]
+    }
+})
+
+const CartItemModel = new mongoose.model('cartItem', CartItemSchema)
+
+module.exports = CartItemModel;
