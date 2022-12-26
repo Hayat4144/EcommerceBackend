dfb0c0c10296a0b4e89828127824b1fc316b9ad0
hayat4144 <ihayat855@gmail.com>
2022-12-26 18:17:37 +0530

    create all product varient at at functionality is  added.

--- Shop/Product/Api/varients/CrProductVarient.js
+++ Shop/Product/Api/varients/CrProductVarient.js
@@ -2,40 +2,87 @@ const ProductVarient_Model = require("../..//model/Product_Varient");
 const ErrorHandler = require("../../../../utils/ErrorHandler");
 const AsyncFunc = require("../../../../utils/AsyncFunc");
 const cloudinary = require('cloudinary').v2
+
 exports.CreateProductVarient = AsyncFunc(async (req, res, next) => {
-    const Data = JSON.parse(req.body.data);
-    const image_files = req.files;
-    const ImageUploadPromise = image_files.map((item) => {
+    const { productId, sizes, colors, stocks, prices } = req.body;
+    const ProductVarientPromise = sizes.map((size => {
         return new Promise((resolve, reject) => {
-            cloudinary.uploader.upload(item.path, { folder: 'Shop' }, (error, data) => {
-                if (error) {
-                    next(new ErrorHandler(error.message, 400))
-                    return reject({ error })
-                }
-                else {
-                    return resolve(data.secure_url)
-                }
+            let id = 0;
+            colors.map((color) => {
+                const varient = new ProductVarient_Model({
+                    product: productId,
+                    stock: stocks[id],
+                    price: prices[id],
+                    seller: req.user_id,
+                    product_attribute: { size: size, color: color },
+                })
+                varient.save((error, data) => {
+                    if (error) {
+                        console.log(error)
+                        reject({ error })
+                    }
+                    else {
+                        console.log(data)
+                        resolve()
+                    }
+                })
+                id++;
+                console.log(id)
             })
         })
-    })
-    await Promise.all(ImageUploadPromise).then((data) => {
-        ProductVarient_Model.create({
-            stock: Data.stock,
-            price: Data.price,
-            product: Data.product,
-            assets: {
-                images: data
-            },
-            product_attribute: Data.attributes,
-            seller: req.user_id
-        }, (error, data) => {
-            if (error) {
-                next(new ErrorHandler(error.message, 400))
-            } else {
-                return res.status(200).json({ data })
-            }
+    }));
+
+    await Promise.all(ProductVarientPromise)
+        .then((data) => {
+            return res.status(200).json({ data: "Product varients has been created successfull." })
         })
-    }).catch((error) => {
-        res.status(400).json({ error })
-    })
+        .catch((error) => {
+            next(new ErrorHandler(error, 400))
+        })
+
+
 });
+
+
+
+// const createVariants = async (productId) => {
+//     let id = 1;
+
+
+//     for (const size of sizes) {
+//         for (const color of colors) {
+//             const variant = new ProductVarient_Model({
+//                 product: productId,
+//                 product_attribute: { size: size, color: color },
+//                 stock: stocks[id - 1],
+//                 price: prices[id - 1],
+//                 // other fields for your variant
+//             });
+//             await variant.save();
+//             id++;
+//         }
+//     }
+// };
+
+const createVariants = async (productId) => {
+
+
+    for (const size of sizes) {
+        let id = 0;
+        for (const color of colors) {
+            const variant = new ProductVarient_Model({
+                product: productId,
+                product_attribute: { size: size, color: color },
+                stock: stocks[id],
+                price: prices[id],
+                // other fields for your variant
+            });
+            console.log(id)
+            await variant.save();
+            id++;
+        }
+    }
+};
+
+
+// createVariants("63a949af0e56b43b06f5507e");
