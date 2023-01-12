2b61f1e28c043f3eb255969bc9f0f847828f88e9
hayat4144 <ihayat855@gmail.com>
2023-01-12 10:13:44 +0530

    sample product fetching .

---
+++ Shop/Product/Api/SampleProducts.js
@@ -0,0 +1,23 @@
+const AsyncFunc = require('../../../utils/AsyncFunc')
+const ErrorHandler = require('../../../utils/ErrorHandler')
+const ProductModel = require('../model/Product_Model')
+
+exports.SampleProducts = (AsyncFunc(async (req, res, next) => {
+    const { search } = req.query;
+    ProductModel.aggregate([
+        {
+            $search: {
+                text: {
+                    query: search,
+                    path: "name",
+                    fuzzy: {
+                        maxEdits: 2
+                    }
+                }
+            }
+        }
+    ], (error, data) => {
+        if (error) return next(new ErrorHandler(error, 400));
+        return res.status(200).json({ data })
+    })
+}))
