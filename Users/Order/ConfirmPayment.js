f1c5f4dc976ebf87d38492f29ac7f10f2eb4aad9
Hayat ilyas <ihayat855@gmail.com>
2023-01-21 18:44:42 +0530

    confirm payment functionality is added.

---
+++ Users/Order/ConfirmPayment.js
@@ -0,0 +1,22 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const AsyncFunc = require("../../utils/AsyncFunc");
+const ErrorHandler = require("../../utils/ErrorHandler");
+
+exports.ConfirmPayment = AsyncFunc(async (req, res, next) => {
+  const { token, payment_intentId } = req.body;
+  await stripe.paymentIntents
+    .confirm(payment_intentId, {
+      payment_method_data: {
+        type: "card",
+        card: {
+          token: token.id,
+        },
+      },
+    })
+    .then((result) => {
+      return res.status(200).json({ data: result });
+    })
+    .catch((error) => {
+      if (error) return next(new ErrorHandler(error.message, 400));
+    });
+});
