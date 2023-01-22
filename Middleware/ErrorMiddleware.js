510a1ef7f52ec51a4acb3218a9831ee346c48e07
Hayat ilyas <ihayat855@gmail.com>
2023-01-22 18:44:42 +0530

    Errore handle properly.

--- Middleware/ErrorMiddleware.js
+++ Middleware/ErrorMiddleware.js
@@ -4,7 +4,8 @@ exports.ErrorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.message = err.message || 'Internal server'
     if (err.name === 'CastError') {
-        const message = `Resource not found: ${err.path} `
+        console.log(err);
+        const message = `Invalid ${err.stringValue} of  ${err.kind} for ${err.path} `
         err = new ErrorHandler(message, 400)
     }
     res.status(err.statusCode).json({ error: err.message })
