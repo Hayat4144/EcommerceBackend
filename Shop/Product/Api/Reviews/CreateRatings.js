41598d6c436ba3580908d8f90e21ccc9871b339e
Hayat ilyas <ihayat855@gmail.com>
2023-01-26 20:25:18 +0530

    make asychrons handling .

--- Shop/Product/Api/Reviews/CreateRatings.js
+++ Shop/Product/Api/Reviews/CreateRatings.js
@@ -9,36 +9,42 @@ exports.CreateRatings = AsyncFunc(async (req, res, next) => {
     if (IsProdcutExist === null) return next(new ErrorHandler('Product does not exist', 400));
     const alreadyRated = IsProdcutExist.ratings_review.find(item => item.user.toString() === req.user_id.toString());
     if (alreadyRated) {
-        ProductModel.updateOne(
-            {
-                ratings_review: {
-                    $elemMatch: alreadyRated
+        try {
+            await ProductModel.updateOne(
+                {
+                    ratings_review: {
+                        $elemMatch: alreadyRated
+                    }
+                }, {
+                $set: {
+                    "ratings_review.$.Star": Star,
+                    "ratings_review.$.comment": comment
                 }
-            }, {
-            $set: {
-                "ratings_review.$.Star": Star,
-                "ratings_review.$.comment": comment
-            }
-        }, { $new: true }, (error, data) => {
-            if (error) return next(new ErrorHandler(error.message, 400))
-
-        })
+            }, { new: true })
+        } catch (error) {
+            return next(new ErrorHandler(error.message,400))
+        }
+     
     }
     else {
-        ProductModel.findByIdAndUpdate(product_id,
-            {
-                $push: {
-                    ratings_review: {
-                        user: req.user_id,
-                        Star,
-                        comment
+        try {
+            await  ProductModel.findByIdAndUpdate(product_id,
+                {
+                    $push: {
+                        ratings_review: {
+                            user: req.user_id,
+                            Star,
+                            comment
+                        }
                     }
-                }
-            }, { $new: true }, (error, data) => {
-                if (error) return next(new ErrorHandler(error.messge, 400))
-            })
+                }, { new: true })
+        } catch (error) {
+            return next(new ErrorHandler(error.message,400))
+        }
+      
     }
     const GetAllRatings = await ProductModel.findById(product_id)
+    
     const total_ratings = GetAllRatings.ratings_review;
     let stars = []
     //  ------------------- push star value in stars array --------------------- //
@@ -55,7 +61,7 @@ exports.CreateRatings = AsyncFunc(async (req, res, next) => {
     const average_rating = Math.round(TotalStars / stars.length)
 
      //  ------------------- update the ratings --------------------- //
-    ProductModel.findByIdAndUpdate(product_id, { average_rating }, { $new: true }, (error, data) => {
+    ProductModel.findByIdAndUpdate(product_id, { average_rating }, { new: true }, (error, data) => {
         if (error) return next(new ErrorHandler(error.message, 400))
         return res.status(200).json({ data: "Thank you for rating and review ." })
     })
