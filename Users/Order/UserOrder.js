381732401223a4bcb07244c30e0d8d0bc27a05b8
Hayat ilyas <ihayat855@gmail.com>
2023-01-27 16:44:17 +0530

    populate product data.

--- Users/Order/UserOrder.js
+++ Users/Order/UserOrder.js
@@ -3,11 +3,28 @@ const ErrorHandler = require("../../utils/ErrorHandler");
 const OrderModal = require("../Model/OrderModal");
 exports.UserOrder = AsyncFunc(async (req, res, next) => {
   await OrderModal.find({ user: req.user_id })
+  .populate({
+    path: 'products.varientId',
+    select: 'name price',
+    populate: {
+      path: 'product',
+      select: 'name description assets'
+    }
+  })
     .then((orders) => {
-      if (orders.length < 1)  return next(new ErrorHandler("you have not ordered anything yet." ,400));
+      if (orders.length < 1)
+        return next(
+          new ErrorHandler("you have not ordered anything yet.", 400)
+        );
       return res.status(200).json({ data: orders });
     })
     .catch((error) => {
       next(new ErrorHandler(error, 400));
     });
 });
+
+// .populate('products.varientId', 'name price')
+//   .exec((err, orders) => {
+//     if (err) return next(new ErrorHandler(err, 400));
+//     return res.status(200).json({ data: orders });
+//   });
