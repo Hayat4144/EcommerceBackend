f7cc769dd198103a6177ac0ffe68707a935b99c0
Hayat ilyas <ihayat855@gmail.com>
2023-01-27 20:27:54 +0530

    unwanted file

--- Users/Profile/ht.js
+++
@@ -1,9 +0,0 @@
-const AsyncFunc = require("../../utils/AsyncFunc");
-const cloudinary = require('cloudinary').v2;
-const streamifier = require('streamifier')
-exports.ht = AsyncFunc(async (req, res, next) => {
-    console.log(req.file)
-    const { data } = req.body;
-    console.log(JSON.parse(data))
-    res.status(200).json(req.file)
-})
