9b212fc51891c73e1e5d09677eee5cbd1590c951
Hayat ilyas <ihayat855@gmail.com>
2023-01-30 12:33:25 +0530

    adding logic to get token based on envirmnent

--- Users/Auth/Logout.js
+++ Users/Auth/Logout.js
@@ -2,7 +2,8 @@ const AsyncFunc = require('../../utils/AsyncFunc')
 const ErrorHandler = require('../../utils/ErrorHandler') 
 
 exports.Logout = AsyncFunc(async(req,res,next)=>{
-    if(!req.cookies.token) return next(new ErrorHandler('You are unauthorized',400)) ;
-    return res.status(200).clearCookie('token', { expires: new Date(0) }).json({data:"Logout Successfull."})
+    const jwt_token = process.env.NODE_ENV === 'production' ? req.cookies.token_production : req.cookies.token_dev ;
+    if(!jwt_token) return next(new ErrorHandler('You are unauthorized',400)) ;
+    return res.status(200).clearCookie(process.env.NODE_ENV === 'production' ? 'token_production' : 'token_dev', { expires: new Date(0) }).json({data:"Logout Successfull."})
 
 })
