f001bc3cc77134423710d7512ab32d38117e854d
Hayat ilyas <ihayat855@gmail.com>
2023-03-06 13:10:51 +0530

    read product api

---
+++ Shop/Product/Api/SellerReadProduct.js
@@ -0,0 +1,64 @@
+const AsyncFunc = require("../../../utils/AsyncFunc");
+const ErrorHandler = require("../../../utils/ErrorHandler");
+const ProductModel = require("../model/Product_Model");
+
+exports.SellerReadProduct = AsyncFunc(async (req, res, next) => {
+    let { search, page } = req.query;
+    const pagination = (resultPerPage) => {
+        page = page || 1
+        const skip = resultPerPage * (page - 1)
+        return skip;
+    }
+
+
+    ProductModel.aggregate([
+        {
+            $search: {
+                index: 'default',
+                text: {
+                    query: search,
+                    path: 'name',
+                    fuzzy: {
+                        maxEdits: 2
+                    }
+                }
+            }
+        },
+        {
+            $match: { seller: req.user_id }
+        },
+        {
+            $facet: {
+                result: [{ $skip: pagination(10) }, { $limit: 10 }],
+                total_result: [
+                    {
+                        $count: 'count'
+                    }
+                ]
+            }
+        }
+    ]).exec((err, doc) => {
+        console.log(err, doc);
+        if (err) return new ErrorHandler(err.message, 400);
+        if (doc.length < 1) return res.status(404).json({ data: 'no product found' })
+        return res.status(200).json({ data: doc[0] })
+    })
+})
+
+
+exports.FetchSellerProduct = AsyncFunc(async (req, res, next) => {
+    let { page } = req.query;
+    const pagination = (resultPerPage) => {
+        page = page || 1
+        const skip = resultPerPage * (page - 1)
+        return skip;
+    }
+
+    ProductModel.find({
+        seller: req.user_id
+    }).skip(pagination(10)).exec((err, doc) => {
+        if (err) return new ErrorHandler(err.message, 400);
+        if (doc.length < 1) return res.status(404).json({ data: 'no product found' })
+        return res.status(200).json({ data: doc})
+    })
+})
