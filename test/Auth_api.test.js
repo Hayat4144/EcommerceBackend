7a53595e0a25357b9a1b2ab5c9221b7a6d8ccd34
Hayat ilyas <ihayat855@gmail.com>
2023-05-17 12:53:52 +0530

    test: testing auth api for signup,signin and logout

--- test/Auth_api.test.js
+++ test/Auth_api.test.js
@@ -1,10 +1,14 @@
 const request = require("supertest");
 const baseURL = "http://localhost:5000";
+const randomEmail = require("random-email");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
-describe("POST Sigupn Endpoint test", () => {
-  afterAll(async () => {
-    await request(baseURL).post("/v3/api/user/signup");
-  });
+jest.mock("jsonwebtoken");
+jest.mock("fs");
+
+describe("POST Signup Endpoint test", () => {
+  afterEach(jest.clearAllMocks);
 
   it("should return 400 for a invalid signup request", async () => {
     const new_user = null;
@@ -18,7 +22,7 @@ describe("POST Sigupn Endpoint test", () => {
     const new_user = {
       firstName: "designer",
       lastName: "momssdf",
-      email: "mom760@gmail.com",
+      email: randomEmail({ domain: "gmail.com" }),
       password: "Hashpassword",
       confirmpassword: "Hashpassword",
     };
@@ -32,7 +36,7 @@ describe("POST Sigupn Endpoint test", () => {
     const new_user = {
       firstName: "designer",
       lastName: "momssdf",
-      email: "mom362@gmail.com",
+      email: randomEmail({ domain: "gmail.com" }),
       password: "Hashpassword(40!",
       confirmpassword: "Hashpassword#43&",
     };
@@ -60,7 +64,7 @@ describe("POST Sigupn Endpoint test", () => {
     const new_user = {
       firstName: "designer",
       lastName: "momssdf",
-      email: "momdesire@gmail.com", // change the email when run 
+      email: randomEmail({ domain: "gmail.com" }),
       password: "Hashpassword@42",
       confirmpassword: "Hashpassword@42",
     };
@@ -68,5 +72,69 @@ describe("POST Sigupn Endpoint test", () => {
       .post("/v3/api/user/signup")
       .send(new_user);
     expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("API endpoints test for signin", () => {
+  test("should return 400 if credential not provide", async () => {
+    const credential = null;
+    const response = await request(baseURL)
+      .post("/v3/api/user/signin")
+      .send(credential);
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should return 400 if the email is not found", async () => {
+    const credential = {
+      email: "test34@gmail.com",
+      password: "skdjflsdjfklsdf",
+    };
+    const response = await request(baseURL)
+      .post("/v3/api/user/signin")
+      .send(credential);
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should return 400 if the credential is not valid", async () => {
+    const credential = {
+      email: "mom80@gmail.com",
+      password: "skdjflsdjfklsdf",
+    };
+    const response = await request(baseURL)
+      .post("/v3/api/user/signin")
+      .send(credential);
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should return 200 if the credential is valid", async () => {
+    const credential = {
+      email: "mom80@gmail.com",
+      password: "Hashpassword@42",
+    };
+
+    const response = await request(baseURL)
+      .post("/v3/api/user/signin")
+      .send(credential);
+    expect(response.get("Set-Cookie")).toBeDefined();
+    expect(response.body.data).toBe("Login Successfull.");
+    expect(response.body.token).toBeDefined();
+    expect(response.statusCode).toBe(200);
+
+    // verify jwt token
+    fs.readFile("./public.pem", "utf8", async (err, data) => {
+      if (!err) {
+        const verify_option = {
+          expiresIn: "10d",
+          algorithm: ["ES256"],
+        };
+        const verify_token = jwt.verify(
+          response.body.token,
+          data,
+          verify_option
+        );
+        expect(verify_token).toBeDefined();
+      }
+    });
+    expect(fs.readFile).toHaveBeenCalledTimes(1);
   });
 });
