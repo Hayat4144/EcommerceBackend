ca46f3005c3d0192ec357b583798356d00a9797d
Hayat ilyas <ihayat855@gmail.com>
2023-05-22 10:22:35 +0530

    fixing some error.

--- Users/Address/api/ReadUserAddress.js
+++ Users/Address/api/ReadUserAddress.js
@@ -6,11 +6,7 @@ const UserModel = require('../../Model/UserModel');
 exports.ReadUserAddress = AsyncFunc(async (req, res, next) => {
 	const IsDataExist = await UserAddressModel.find({ user: req.user_id }).clone();
 	if (IsDataExist.length === 0) {
-		return next(new ErrorHandler('You have not been added any address.', 400))
-
+		return res.status(400).json({error:"You have been not added any address yet."})
 	}
 	return res.status(200).json({ data: IsDataExist })
-
-
-
 })	
