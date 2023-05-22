87a48583f9c6a37dfb6f4d70919b5162cdce23e8
Hayat ilyas <ihayat855@gmail.com>
2023-05-22 08:05:41 +0530

    change the form sync to async for better performance.

--- utils/Send_mail.js
+++ utils/Send_mail.js
@@ -1,26 +1,28 @@
 const nodemailer = require("nodemailer");
 const ErrorHandler = require("./ErrorHandler");
 
-exports.Send_mail = async (res, next, response_message, reciever_email, subject, message) => {
+exports.Send_mail = async (email, subject, message) => {
+  try {
     let transporter = nodemailer.createTransport({
-        service: process.env.SMTP_SERVICE,
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false,
-        auth: {
-            user: process.env.SMTP_EMAIL_ADDRESS,
-            pass: process.env.PASSWORD,
-        },
+      service: process.env.SMTP_SERVICE,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_EMAIL_ADDRESS,
+        pass: process.env.PASSWORD,
+      },
     });
-
     let mailoption = {
-        from: process.env.SMTP_EMAIL_ADDRESSs,
-        to: reciever_email,
-        subject: subject,
-        html: message,
+      from: process.env.SMTP_EMAIL_ADDRESSs,
+      to: email,
+      subject: subject,
+      html: message,
     };
 
-    transporter.sendMail(mailoption, (err, info) => {
-        return err ? next(new ErrorHandler(err, 400)) : res.status(200).json({ data: response_message });
-    });
+    const info = await transporter.sendMail(mailoption);
+    return { info };
+  } catch (error) {
+    return { error };
+  }
 };
