d74e135244d4993156f91f9cd51d75a1cc99ac84
Hayat ilyas <ihayat855@gmail.com>
2023-05-22 20:34:03 +0530

    similar product feature is improved .

--- Shop/Product/Api/SimilarProducts.js
+++ Shop/Product/Api/SimilarProducts.js
@@ -1,30 +1,48 @@
-const AsyncFunc = require('../../../utils/AsyncFunc')
-const ErrorHandler = require('../../../utils/ErrorHandler')
-const ProductModel = require('../model/Product_Model')
-const mongoose = require('mongoose')
+const AsyncFunc = require("../../../utils/AsyncFunc");
+const ErrorHandler = require("../../../utils/ErrorHandler");
+const ProductModel = require("../model/Product_Model");
+const mongoose = require("mongoose");
 
-exports.SimilarProducts = (AsyncFunc(async (req, res, next) => {
-    const { search, productId, categoryId } = req.query;
-    ProductModel.aggregate([
-        {
-            $search: {
-                text: {
-                    query: search,
-                    path: "name",
-                    fuzzy: {
-                        maxEdits: 2
-                    }
-                }
-            }
+exports.SimilarProducts = AsyncFunc(async (req, res, next) => {
+  const { search, productId, category } = req.query;
+  ProductModel.aggregate(
+    [
+      {
+        $search: {
+          text: {
+            query: search,
+            path: "name",
+            fuzzy: {
+              maxEdits: 2,
+            },
+          },
         },
-        {
-            $match: {
-                category: mongoose.Types.ObjectId(categoryId),
-                _id: { $ne: mongoose.Types.ObjectId(productId) }
-            }
-        }
-    ], (error, data) => {
-        if (error) return next(new ErrorHandler(error, 400));
-        return res.status(200).json({ data })
-    })
-}))
+      },
+      {
+        $match: {
+          category: {
+            $regex: new RegExp(`^${category}`),
+            $options: "i",
+          },
+          _id: { $ne: mongoose.Types.ObjectId(productId) },
+        },
+      },
+      {
+        $facet: {
+          result: [{ $limit: 30 }],
+          totalResult: [
+            {
+              $count: "count",
+            },
+          ],
+        },
+      },
+    ],
+    (error, data) => {
+      if (error) return next(new ErrorHandler(error, 400));
+      if(data[0].result.length < 1) return res.status(400).json({error:"No similar product has been found."})
+      return res.status(200).json({ data :data[0]});
+    }
+  );
+});
+
