9213fffabc8e3a426c4242be73b20df6f8d3c32c
Hayat ilyas <ihayat855@gmail.com>
2023-05-22 09:38:46 +0530

    Adding Asyncfun eror for error handling.

--- Seller/auth/SigninSelller.js
+++ Seller/auth/SigninSelller.js
@@ -4,63 +4,60 @@ const bcrypt = require('bcrypt')
 const ErrorHandler = require('../../utils/ErrorHandler')
 const fs = require('fs')
 const path = require('path')
+const AsyncFunc = require('../../utils/AsyncFunc')
 
 
-exports.SigninSeller = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const IsSellerExist = await Seller_Model.findOne({ email: email });
-        if (!IsSellerExist) {
-            return next(new ErrorHandler('Sorry, Invalid/Credentials', 400))
-        }
-        if (IsSellerExist && (await bcrypt.compare(password, IsSellerExist.password))) {
-
-            // signinoptions for jwt sign
-            var signOptions = {
-                expiresIn: "12d",
-                algorithm: "ES256"
-            };
-
-            // jwt payload
-            const payload = {
-                id: IsSellerExist._id,
-                email: IsSellerExist.email,
-                name: IsSellerExist.name,
-                store_name: IsSellerExist.store_name,
-                is_varified: IsSellerExist.is_varified,
-            }
-
-            // read the private key file and sign a token if no error occured
-            fs.readFile('./private.ec.key', 'utf-8', (err, data) => {
-                if (!err) {
-                    const token = jwt.sign(payload, data, signOptions);
-                    if (process.env.NODE_ENV === "production") {
-                        res.cookie('token_production', token,{
-                            expires: new Date(Date.now() + 864000000), // for 10 days in production only 864000000
-                            sameSite: 'none',
-                            secure:true
-                         }
-                         )
-                    }else{
-                        res.cookie('token_dev', token, {
-                            expires: new Date(Date.now() + 864000000), // for 10 days in production only 864000000
-                            sameSite: 'none',
-                        })
-                    }
-                    return res.status(200).send({ data: 'Login Successfully.' ,token })
-                }
-                else {
-                    throw err;
-                }
-            })
-
-
-        }
-        else {
-            return next(new ErrorHandler('Sorry, Invalid/Credentials', 400))
-        }
-
-    } catch (error) {
-        console.log(error)
+exports.SigninSeller = AsyncFunc(async (req, res, next) => {
+    const { email, password } = req.body;
+    const IsSellerExist = await Seller_Model.findOne({ email: email });
+    if (!IsSellerExist) {
+        return next(new ErrorHandler('Sorry, Invalid/Credentials', 400))
     }
-}
+    if (IsSellerExist && (await bcrypt.compare(password, IsSellerExist.password))) {
+
+        // signinoptions for jwt sign
+        var signOptions = {
+            expiresIn: "12d",
+            algorithm: "ES256"
+        };
+
+        // jwt payload
+        const payload = {
+            id: IsSellerExist._id,
+            email: IsSellerExist.email,
+            name: IsSellerExist.name,
+            store_name: IsSellerExist.store_name,
+            is_varified: IsSellerExist.is_varified,
+        }
+
+        // read the private key file and sign a token if no error occured
+        fs.readFile('./private.ec.key', 'utf-8', (err, data) => {
+            if (!err) {
+                const token = jwt.sign(payload, data, signOptions);
+                if (process.env.NODE_ENV === "production") {
+                    res.cookie('token_production', token, {
+                        expires: new Date(Date.now() + 864000000), // for 10 days in production only 864000000
+                        sameSite: 'none',
+                        secure: true
+                    }
+                    )
+                } else {
+                    res.cookie('token_dev', token, {
+                        expires: new Date(Date.now() + 864000000), // for 10 days in production only 864000000
+                        sameSite: 'none',
+                    })
+                }
+                return res.status(200).send({ data: 'Login Successfully.', token })
+            }
+            else {
+                throw err;
+            }
+        })
+
+
+    }
+    else {
+        return res.status(400).json({error:`Sorry, Invalid/Credentials`})
+    }
+
+})
