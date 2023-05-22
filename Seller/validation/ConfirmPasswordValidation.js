a52bec868a4107e45edbb3ec2683d585d8d1722b
Hayat ilyas <ihayat855@gmail.com>
2023-05-22 08:58:35 +0530

    bug: bcrypt hash not found bug fixed with validation.

--- Seller/validation/ConfirmPasswordValidation.js
+++ Seller/validation/ConfirmPasswordValidation.js
@@ -1,36 +1,36 @@
-const { check, validationResult } = require('express-validator')
-
+const { check, validationResult } = require("express-validator");
 
 exports.ConfrimPasswordValidate = [
-    check('newpassword')
-        .isStrongPassword(
-            {
-                minLength: 8,
-                minLowercase: 1,
-                minUppercase: 1,
-                minNumbers: 1,
-                minSymbols: 1,
-              }
-        )
-        .trim()
-        .escape()
-        .withMessage('password must be contain symbol,number,capital and small letter.')
-        .custom((value, { req, loc, path }) => {
-            if (value !== req.body.confirmpassword) {
-                throw new Error("password doesn't match");
-            } else {
-                return value;
-            }
-        }),
-
-]
+  check("newpassword")
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .trim()
+    .escape()
+    .withMessage(
+      "password must be contain symbol,number,capital and small letter having 8 character."
+    )
+    .custom((value, { req, loc, path }) => {
+      if (value !== req.body.confirmpassword) {
+        throw new Error("password doesn't match");
+      } else {
+        return value;
+      }
+    }),
+  check("currentpassword")
+    .isLength({min:1})
+    .withMessage(`old password can't be empty.`)
+];
 
 exports.Confirm_Password_Validation_Error = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(400).json({ error: errors.array()[0].msg })
-    }
-    else {
-        next();
-    }
-}
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ error: errors.array()[0].msg });
+  } else {
+    next();
+  }
+};
--- Users/Auth/UserChangePassword.js
+++ Users/Auth/UserChangePassword.js
@@ -1,36 +1,34 @@
-const UserModel = require('../Model/UserModel')
-const bcrypt = require('bcrypt')
-const ErrorHandler = require('../../utils/ErrorHandler');
-const AsyncFunc = require('../../utils/AsyncFunc');
-
-
+const UserModel = require("../Model/UserModel");
+const bcrypt = require("bcrypt");
+const ErrorHandler = require("../../utils/ErrorHandler");
+const AsyncFunc = require("../../utils/AsyncFunc");
 
 exports.UserChangePassword = AsyncFunc(async (req, res, next) => {
     const { currentpassword, newpassword } = req.body;
-    await UserModel.findById(req.user_id)
-        .exec(async (err, doc) => {
-            if (err) {
-                next(new ErrorHandler('Sorry , the data does not exist.', 400))
+    UserModel.findById(req.user_id).exec(async (err, doc) => {
+        if (err) {
+            return res.status(400).json({ error: "Sorry,User does not exist." });
+        }
+        if (!(await bcrypt.compare(currentpassword, doc.password))) {
+            return res.status(400).json({ error: "Sorry, Invalid password" });
+        }
+        const saltRound = 10;
+        const hashpassword = await bcrypt.hash(newpassword, saltRound);
+        await UserModel.findByIdAndUpdate(
+            doc._id,
+            { $set: { password: hashpassword } },
+            { $new: true }
+        ).exec((error, doc) => {
+            if (error) {
+                next(new ErrorHandler(error.message, 400));
+            } else {
+                if (doc === null) {
+                    return res.status(400).json({ error: `Sorry , User does not exist` });
+                }
+                return res
+                    .status(200)
+                    .json({ data: "The password has been changed successfully." });
             }
-            if (await bcrypt.compare(currentpassword, doc.password)) {
-                const saltRound = 10;
-                const hashpassword = await bcrypt.hash(newpassword, saltRound);
-                await UserModel.findByIdAndUpdate(doc._id, { $set: { password: hashpassword } }, { $new: true })
-                    .exec((error, doc) => {
-                        if (error) {
-                            next(new ErrorHandler(error.message, 400))
-                        }
-                        else {
-                            if (doc === null) {
-                                return res.status(400).json({ 'error': `Sorry , User does not exist` })
-                            }
-                            return res.status(200).json({ data: "The password has been changed successfully." })
-                        }
-                    })
-            }
-            else {
-                next(new ErrorHandler('Invalid password', 400))
-            }
-        })
-
-})
+        });
+    });
+});
