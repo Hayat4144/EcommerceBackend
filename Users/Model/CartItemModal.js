0e746201987b48d3281588acf2b792360d3b96a9
Hayat ilyas <ihayat855@gmail.com>
2023-05-23 12:12:11 +0530

    adding cartitem modal

--- Users/Model/CartItemModal.js
+++ Users/Model/CartItemModal.js
@@ -1,28 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const CartItemSchema = new mongoose.Schema({
-    cartId: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: true,
-        ref: 'cart'
-    },
-    ProductvarientId: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: true,
-        ref: 'product_varients'
-    },
-    quantity: {
-        type: Number,
-        required: [true, "Please Provide the Quantity"],
-        max: [500, "You should have to buy maximum 500"],
-        min: [5, "You should have to buy atleast 5"]
-    },
-    price: {
-        type: Number,
-        required: [true, "Please provide the price"]
-    }
-})
+  CartId: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true,
+    ref: "cart",
+  },
+  ProductId: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true,
+    ref: "Product",
+  },
+  ProductvarientId: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "product_varients",
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Please Provide the Quantity"],
+    max: [500, "You should have to buy maximum 500"],
+    min: [5, "You should have to buy atleast 5"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please provide the price"],
+  },
+});
 
-const CartItemModel = new mongoose.model('cartItem', CartItemSchema)
+const CartItemModel = new mongoose.model("cartItem", CartItemSchema);
 
 module.exports = CartItemModel;
