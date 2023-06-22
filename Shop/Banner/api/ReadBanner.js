438ed0f33f0aefb6e3d5ed27baa16b5f5a014a10
Hayat ilyas <ihayat855@gmail.com>
2023-06-22 14:08:53 +0530

    removing next handler.

--- Shop/Banner/api/ReadBanner.js
+++ Shop/Banner/api/ReadBanner.js
@@ -1,15 +1,15 @@
-const AyscnFun = require('../../../utils/AsyncFunc')
-const ErrorHandler = require('../../../utils/ErrorHandler')
-const BannerModel = require('../Model/BannerModel')
+const AyscnFun = require("../../../utils/AsyncFunc");
+const ErrorHandler = require("../../../utils/ErrorHandler");
+const BannerModel = require("../Model/BannerModel");
 
 exports.ReadBanner = AyscnFun(async (req, res, next) => {
-    BannerModel.find({}).exec((error, data) => {
-        if (!error) {
-            if (data.length === 0) {
-                next(new ErrorHandler('Sorry no Banner found', 400))
-            }
-            return res.status(200).json({ data })
-        }
-        next(new ErrorHandler(error, 400))
-    })
-})
+  BannerModel.find({}).exec((error, data) => {
+    if (!error) {
+      if (data.length === 0) {
+        return res.status(400).json({ error: "Sorry,No Banner found." });
+      }
+      return res.status(200).json({ data });
+    }
+    return next(new ErrorHandler(error, 400));
+  });
+});
