21a64abfb045f279081212c387707faf38475a55
Hayat ilyas <ihayat855@gmail.com>
2023-06-22 15:32:58 +0530

    adding credential.

--- Users/Auth/Logout.js
+++ Users/Auth/Logout.js
@@ -1,9 +1,18 @@
-const AsyncFunc = require('../../utils/AsyncFunc') 
-const ErrorHandler = require('../../utils/ErrorHandler') 
+const AsyncFunc = require("../../utils/AsyncFunc");
+const ErrorHandler = require("../../utils/ErrorHandler");
 
-exports.Logout = AsyncFunc(async(req,res,next)=>{
-    const jwt_token = process.env.NODE_ENV === 'production' ? req.cookies.token : req.cookies.token_dev ;
-    if(!jwt_token) return next(new ErrorHandler('You are unauthorized',400)) ;
-    return res.status(200).clearCookie(process.env.NODE_ENV === 'production' ? 'token' : 'token_dev', { expires: new Date(0) }).json({data:"Logout Successfull."})
-
-})
+exports.Logout = AsyncFunc(async (req, res, next) => {
+  const jwt_token =
+    process.env.NODE_ENV === "production"
+      ? req.cookies.token
+      : req.cookies.token_dev;
+  if (!jwt_token) return next(new ErrorHandler("You are unauthorized", 400));
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  return res
+    .status(200)
+    .clearCookie(
+      process.env.NODE_ENV === "production" ? "token" : "token_dev",
+      { expires: new Date(0) }
+    )
+    .json({ data: "Logout Successfull." });
+});
