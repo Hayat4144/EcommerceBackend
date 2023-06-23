7e8165e5e407cee4a06744bb9190dbe1e1360832
Hayat ilyas <ihayat855@gmail.com>
2023-06-23 15:33:23 +0530

    fixing logout functionality.

--- Middleware/UserAuthMiddleware.js
+++ Middleware/UserAuthMiddleware.js
@@ -4,6 +4,7 @@ const User_Model = require("../Users/Model/UserModel");
 const ErrorHandler = require("../utils/ErrorHandler");
 const jwt_signin_verify_option = require("../global/Jwt_verify_signin_option");
 const logger = require("../utils/Logger");
+const path = require("path");
 
 const UserAuthMiddleware = async (req, res, next) => {
   try {
@@ -16,30 +17,36 @@ const UserAuthMiddleware = async (req, res, next) => {
       return res.status(401).json({ error: "you are unauthorized." });
     }
     const file = path.join(process.cwd(), "public.pem");
-    const cert = fs.readFileSync(file);
-    jwt.verify(
-      jwt_token,
-      cert,
-      jwt_signin_verify_option,
-      async (error, data) => {
-        if (error) {
-          const message =
-            error.name === "JsonWebTokenError"
-              ? "you are unauthorized"
-              : error.message;
-          return next(new ErrorHandler(message, 401));
+    fs.readFile(file, "utf-8", (err, data) => {
+      if (err) return res.status(500).json({ error: err.message });
+      jwt.verify(
+        jwt_token,
+        data,
+        jwt_signin_verify_option,
+        async (error, data) => {
+          if (error) {
+            const message =
+              error.name === error.message
+                ? "you are unauthorized"
+                : error.message;
+            return next(new ErrorHandler(message, 401));
+          }
+          await User_Model.findById(data.id).exec((err, doc) => {
+            if (err) {
+              return res.status(401).json({ error: "you are unauthorised" });
+            }
+            if (doc === null)
+              return next(
+                new ErrorHandler("you data has not been found.", 401)
+              );
+            req.user_id = doc._id;
+            req.email = doc.email;
+            req.name = data.name;
+            next();
+          });
         }
-        await User_Model.findById(data.id).exec((err, doc) => {
-          if (err) return next(new ErrorHandler("you are unauthorised", 401));
-          if (doc === null)
-            return next(new ErrorHandler("you data has not been found.", 401));
-          req.user_id = doc._id;
-          req.email = doc.email;
-          req.name = data.name;
-          next();
-        });
-      }
-    );
+      );
+    });
   } catch (error) {
     logger.error(error);
   }
--- Users/Auth/Logout.js
+++ Users/Auth/Logout.js
@@ -1,10 +1,10 @@
 const AsyncFunc = require("../../utils/AsyncFunc");
-const ErrorHandler = require("../../utils/ErrorHandler");
 
 exports.Logout = AsyncFunc(async (req, res, next) => {
-  const token = (process.env.NODE_ENV = "production"
-    ? req.cookies.token
-    : req.cookies.token_dev);
+  const token =
+    process.env.NODE_ENV === "production"
+      ? req.cookies.token
+      : req.cookies.token_dev;
   if (!token) {
     return res.status(401).json({ error: "you are unauthorized." });
   }
@@ -13,5 +13,6 @@ exports.Logout = AsyncFunc(async (req, res, next) => {
     .clearCookie(
       process.env.NODE_ENV === "production" ? "token" : "token_dev",
       { expires: new Date(0) }
-    );
+    )
+    .json({ data: "Logout successful." });
 });
