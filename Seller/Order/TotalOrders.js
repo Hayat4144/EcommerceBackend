dc2f964d67c1250edb0816c75294e8c9d5f10335
Hayat ilyas <ihayat855@gmail.com>
2023-06-26 08:47:30 +0530

    fixing today order by adding hours formate to date .

--- Seller/Order/TotalOrders.js
+++ Seller/Order/TotalOrders.js
@@ -1,20 +1,31 @@
 const AsyncFunc = require("../../utils/AsyncFunc");
 const OrderModal = require("../../Users/Model/OrderModal");
-const ErrorHandler = require('../../utils/ErrorHandler')
+const ErrorHandler = require("../../utils/ErrorHandler");
 
 exports.TotalOrders = AsyncFunc(async (req, res, next) => {
-    OrderModal.find({ "products.seller": req.user_id }).count().exec((err, doc) => {
-        if (err) return next(new ErrorHandler(err.message, 400));
-        return res.status(200).json({ data: doc })
-    })
-})
-
+  OrderModal.find({ "products.seller": req.user_id })
+    .count()
+    .exec((err, doc) => {
+      if (err) return next(new ErrorHandler(err.message, 400));
+      return res.status(200).json({ data: doc });
+    });
+});
 
 exports.TodayOrders = AsyncFunc(async (req, res, next) => {
-    let today_date = new Date();
-    today_date.toISOString()
-    OrderModal.find({ 'products.seller': req.user_id, 'date': today_date }).count().exec((error,doc)=>{
-        if(error) return next(new ErrorHandler(error.message,400));
-        return res.status(200).json({data:doc})
-    })
-})
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+
+  OrderModal.find({
+    "products.seller": req.user_id,
+    "date": {
+      $gte: startOfDay,
+      $lte: endOfDay,
+    },
+  }).count().exec((error, doc) => {
+    if (error) return next(new ErrorHandler(error.message, 400));
+    return res.status(200).json({ data: doc });
+  });
+});
